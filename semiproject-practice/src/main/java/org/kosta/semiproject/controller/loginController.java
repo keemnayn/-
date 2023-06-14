88523c0329d5272decd34d23aa5fa45bfc5a1e28
jung97 <eprtms3@naver.com>
2023-06-14 17:26:47 +0900

    추가 loginController

---
+++ semiproject-practice/src/main/java/org/kosta/semiproject/controller/loginController.java
@@ -0,0 +1,13 @@
+package org.kosta.semiproject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class loginController implements Controller {
+
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse reponse) throws Exception {
+		return null;
+	}
+
+}
